5254f7a52dacfa094d652ee19fcc29e59404b7ea
TiamatCod3 <ti_matos@id.uff.br>
2024-06-21 09:52:29 -0300

    scrapper atualizado

--- scrapper.py
+++ scrapper.py
@@ -1,29 +1,39 @@
-import requests
-import json
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import requests, json
 from bs4 import BeautifulSoup
-import csv
+import pandas as pd
+import re
 import sys
 import io
-import pandas as pd
 
 # Configurando a saída padrão para UTF-8
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+# sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+# O trabalho consiste na extraçaõ de notícias do site lance
+# O site tem uma estrutura interessante com elementos claros para se obter os links
+# A estratégia é buscar os links das matérias com suas slugs na página principal e depois buscar os links das matérias nos caminhos dos clubes a serem explorados
+# Ao extrair os links, novamente é feita uma iteração para buscar os conteúdos dos links pelas tags html com a seguinte estrutura: Título Principal(H1), Título Secundário(H2), conteúdo (p) e data da publicação (time)
+# Por quetões de espaço e volume de dados, foram extraídas 5 notícias de cada time e da página principal podendo variar de acordo com a definição variável
 
-# Especificando a url do site
+#Especificando a url do site
 url_principal = "https://www.lance.com.br/"
 
-# Criando a array de times no formato de slugs para iteração
+#Criando a array de times no formato de slugs para iteração
 times = [
-    "atletico-mineiro", "atletico-paranaense", "bahia", "botafogo", "bragantino", "corinthians", "criciuma", "cruzeiro", "cuiaba", "flamengo", "fluminense", "fortaleza", "gremio", "internacional", "palmeiras", "santos", "sao-paulo", "vasco", "vitoria"
+   "atletico-mineiro", "atletico-paranaense","bahia","botafogo", "bragantino",  "corinthians", "criciuma", "cruzeiro", "cuiaba","flamengo", "fluminense", "fortaleza", "gremio", "internacional", "palmeiras", "santos", "sao-paulo", "vasco", "vitoria"
 ]
 
-# Definindo número máximo de notícias por página
-maximo_noticias = 5
+#Definindo número máximo de notícias por página
+maximo_noticias = 10
 
-# Criação da variável onde os links serão salvos
+#Criação da varíavel onde os links serão salvos
 links_gerais = []
 
-# Criação da função para obtenção dos links das páginas de acordo com a url
+#Criação da função para obtenção dos links das páginas de acordo com a url
 def obter_links(url, n_noticias=None):
     # Criando array vazia para coleta dos links
     links = []
@@ -31,7 +41,7 @@ def obter_links(url, n_noticias=None):
     # Criando o contador de notícias
     contador = 0
     
-    # Obtendo os links da página principal usando o requests
+    #Obtendo os links da página principal usando o requests
     response = requests.get(url)
 
     # Verificando se a requisição foi bem-sucedida
@@ -67,6 +77,48 @@ def obter_links(url, n_noticias=None):
     # Retornando a lista de links
     return links
 
+
+
+# In[2]:
+
+
+# Função para extrair data e hora
+def extrair_data_hora(data_str):
+    match = re.search(r'Publicada em (\d{2}/\d{2}/\d{4} - \d{2}:\d{2})', data_str)
+    if match:
+        return match.group(1)
+    return None
+
+# Função para extrair local
+def extrair_local(data_str):
+    match = re.search(r'• Publicada em \d{2}/\d{2}/\d{4} - \d{2}:\d{2} • (.+)', data_str)
+    if match:
+        return match.group(1)
+    return None
+
+
+# In[13]:
+
+
+def validar_estrutura(dicionario, chaves_esperadas):
+    
+    if dicionario is None:
+        return False
+    # Checando se todas as chaves esperadas estão presentes
+    if not chaves_esperadas.issubset(dicionario.keys()):
+        return False
+    
+    # Checando se os valores não são nulos
+    for chave in chaves_esperadas:
+        if dicionario[chave] is None or (isinstance(dicionario[chave], list) and not dicionario[chave]):
+            return False
+    
+    return True
+
+
+# In[3]:
+
+
 def obter_conteudo(url):
     # Criando o dicionário vazio para coletar o conteúdo 
     conteudo = {}
@@ -77,7 +129,7 @@ def obter_conteudo(url):
     # Verificando a resposta
     if article_response.status_code == 200:
                 
-                # Parseando o conteúdo HTML da página da notícia
+                # Parseanso o conteúdo HTML da página da notícia
                 article_soup = BeautifulSoup(article_response.content, 'html.parser')
                 
                 # Extraindo o conteúdo da tag <article>
@@ -87,22 +139,25 @@ def obter_conteudo(url):
                 if not article:
                      return 
                 
-
                 if not article.find('h1'):
-                     return
+                     return {}
                 # Buscando dentro do artigo o título principal e extraindo o texto pelo get_text
-                conteudo["Título 1"] = article.find('h1').get_text() 
+                conteudo["Título 1"] = article.find('h1').get_text()
 
                 if not article.find('h2'):
-                     return
+                     return {}
+
                 # Buscando dentro do artigo o título principal e extraindo o texto pelo get_text
-                conteudo["Título 2"] = article.find('h2').get_text() if article.find('h2') else "N/A"
+                conteudo["Título 2"] = article.find('h2').get_text()
 
+                if not article.find('time'):
+                     return {}
 
-                if not article.find('Data'):
-                     return
                 # Recuperando os dados de data e local da publicação na tag <time>
-                conteudo["Data"] = article.find('time').get_text() if article.find('time') else "N/A"
+                conteudo["Data"] = extrair_data_hora(article.find('time').get_text())
+                conteudo["Local"] = extrair_local(article.find('time').get_text())
+
+                
 
                 # Encontrando as tags de parágrafos
                 paragrafos = article.find_all('p', class_="w-full")
@@ -110,15 +165,22 @@ def obter_conteudo(url):
                 # Recuperando a array de parágrafos obtidos
                 paragrafos = [p for p in paragrafos if not p.find('a')]
 
-                # Criando uma string vazia para inserção do conteúdo
+                #Criando uma string vazia para inserção do conteúdo
                 conteudo["Conteúdo"] = ""
                 for paragrafo in paragrafos:
+                
                      # Concatenando as strings inserindo a quebra de linha no final como \n
                      conteudo["Conteúdo"] += paragrafo.get_text() + "\n"
+                # conteudo["Conteúdo"] = [conteudo["Conteúdo"]]
     return conteudo
 
+
+
+# In[ ]:
+
+
 # Obtendo links da página principal:
-# Chamando a função para obtenção dos links com 5 links
+# Chamando a função par obtenção dos links com 5 links
 links = obter_links(url_principal, maximo_noticias)
 # Concatenando os novos links recuperados na array de todos os links
 links_gerais += links
@@ -126,7 +188,7 @@ links_gerais += links
 # Imprimindo os tamanhos da array para verificação
 print(len(links_gerais))
 
-# Obtendo os links nas páginas dos times
+# Obtendo os links nas paginas dos times
 # Iterando pelo tamanho da array de times
 for i in range(len(times)):
     # Obtendo os links por time ao concatenar a string da url principal com a string da slug do time e 5 links
@@ -141,35 +203,39 @@ for i in range(len(times)):
 # Convertendo a lista de links gerais em um conjunto para remover as duplicatas
 links_gerais = set(links_gerais)
 
+
+# In[14]:
+
+
+# Definindo as chaves esperadas
+colunas_esperadas  = {"Título 1", "Título 2", "Data", "Local", "Conteúdo"}
+# noticias = pd.DataFrame(columns=colunas)
+# print(noticias)
+noticias = []
+for link in links_gerais:
+     conteudo = obter_conteudo(link)
+     if validar_estrutura(conteudo, colunas_esperadas):
+          noticias.append(conteudo)
+    #  print(conteudo)
+    #  conteudo = pd.DataFrame(conteudo)
+    #  noticias = pd.concat([noticias, conteudo], ignore_index=True)
+
+noticias = pd.DataFrame(noticias)
+
+# noticias.to_csv(arquivo,encoding="utf-8", index=False)
+
+
+# In[15]:
+
+
+noticias["Data"] = pd.to_datetime(noticias["Data"])
+noticias
+
+
+# In[16]:
+
+
 # Declarando o nome do arquivo a salvar os conteúdos extraídos
-arquivo_csv = "noticias_lance.csv"
+arquivo = "noticias_lance.csv"
+noticias.to_csv(arquivo,encoding="utf-8", index=False)
 
-
-# # Abrindo o arquivo CSV para escrita e certificando o encoding
-# with open(arquivo_csv, 'w', newline='', encoding='utf-8') as csvfile:
-#     # Definindo os nomes das colunas
-#     fieldnames = ["Título 1", "Título 2", "Conteúdo", "Data"]
-
-#     # Criando o writer do CSV
-#     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-#     # Escrevendo o cabeçalho
-#     writer.writeheader()
-
-#     # Iterando pelos links
-#     for link in links_gerais:
-#         # Imprimindo os links para verificação
-#         print(link)
-
-#         # Recuperando o dicionário do conteúdo de acordo com o retorno da função
-#         conteudo = obter_conteudo(link)
-#         print(conteudo)
-
-#         # Eliminando conteúdos vazios e não salvando
-#         if conteudo == {} or conteudo is None:
-#             continue
-
-#         # Escrevendo o conteúdo no arquivo CSV
-#         writer.writerow(conteudo)
-
-# print(f'Dados salvos no arquivo {arquivo_csv} com sucesso!')
